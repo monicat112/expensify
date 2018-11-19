428ab7a3d3822fddd93e533af56ac0fd7a41af36
Monica Salter <salter@smartdeploy.com>
2018-11-19 11:19:11 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Start a Project
+
+The main project  
+
+`yarn run build:dev`  
+`yarn run dev-server`
+
+`yarn run build:prod`  
+`yarn run serve`  
+
+A playground project  
+`yarn run ???`
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+// exporting a function https://webpack.js.org/configuration/configuration-types/#exporting-a-function
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  const CSSExtract = new ExtractTextPlugin('styles.css')
+
+  return {
+
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+            use: [
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+            ]
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      open: true,
+      openPage: '/'
+    }
+  }
+}
