199842a6f253e50c1359e2263bc845e0c0106d0b
Monica Salter <salter@smartdeploy.com>
2018-11-29 12:38:09 -0800

    finished isolated redux project

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -130,8 +130,24 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
 
 // Get Visible Expenses
 
-const getVisibleExpenses = (expenses, filters) => {
-    return expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        // only filter by start date if the startDate IS a number
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
+
+        return startDateMatch && endDateMatch && textMatch
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1
+        }
+        else if ( sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1
+        } else {
+            return 0
+        }
+    })
 }
 
 // Store Creation
@@ -146,23 +162,23 @@ const store = createStore(
 store.subscribe(() => {
     const state = store.getState()
     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-    console.log(store.getState()) 
+    console.log(visibleExpenses) 
 }) 
 
 // Dispatching
 
-// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 1000 }))
-// const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -2000 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 3000, createdAt: 1000 }))
 
 // // store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
 
-// store.dispatch(setTextFilter('rent'))
+// store.dispatch(setTextFilter('coffee'))
 // store.dispatch(setTextFilter())
 
-// store.dispatch(sortByAmount()) 
+store.dispatch(sortByAmount()) 
 // store.dispatch(sortByDate())
 
-store.dispatch(setStartDate(125))
-store.dispatch(setStartDate())
-store.dispatch(setEndDate(521))
+// store.dispatch(setStartDate(0))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(3000))
