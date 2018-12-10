7a3c8ee691f4dd470c536006ed8cf8dcf4c97bb2
Monica Salter <salter@smartdeploy.com>
2018-12-10 14:10:21 -0800

    testing the filters reducer

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -34,7 +34,7 @@ test('should filter by text value', () => {
     ])
 })
 
-test('should filter by start date', () => {
+test('should filter by startDate', () => {
     const filters = {
         text: '',
         sortBy: 'date',
@@ -47,15 +47,41 @@ test('should filter by start date', () => {
     ])
 })
 
-test('should filter by end date', () => {
+test('should filter by endDate', () => {
   const filters = {
     text: '',
     sortBy: 'date',
     startDate: undefined,
-    endDate: moment(0)
+    endDate: moment(0).add(2, 'days')
   }
   const result = selectExpenses(expenses, filters)
   expect(result).toEqual([
     expenses[0], expenses[1]
   ])
+})
+
+test('should sort by date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    }
+    const result = selectExpenses(expenses, filters)
+    expect(result).toEqual([
+        expenses[2], expenses[0], expenses[1]
+    ])
+})
+
+test('should sort by amount', () => {
+    const filters = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: 0
+    }
+    const result = selectExpenses(expenses, filters)
+    expect(result).toEqual([
+        expenses[1], expenses[2], expenses[0]
+    ])
 })
