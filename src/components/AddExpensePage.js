4631325ba18d1c25ae3cdb72cf572a5e726a51bb
Monica Salter <salter@smartdeploy.com>
2018-12-20 08:49:53 -0800

    testing the add expense page

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,16 +3,27 @@ import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
 import { addExpense } from '../actions/expenses'
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense (Create)</h1>
-        <ExpenseForm 
-            onSubmit={(expense) => {
-                props.dispatch(addExpense(expense))
-                props.history.push('/')
-            }}
-        />
-    </div>
-)
+export class AddExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        // this.props.dispatch(addExpense(expense))
+        this.props.onSubmit(expense)
+        this.props.history.push('/')
+    }
+    render() {
+        return (
+            <div>
+                <h1>Add Expense (Create)</h1>
+                <ExpenseForm 
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        )
+    }
+}
 
-export default connect()(AddExpensePage)
+// mapDispatchToProps defines props that call dispatch
+const mapDispatchToProps = (dispatch) => ({
+    onSubmit: (expense) => dispatch(addExpense(expense))
+})
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage)
---
+++ src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { AddExpensePage } from '../../components/AddExpensePage'
+
+test('should render AddExpensePage correctly', () => {
+    const onSubmit = jest.fn()
+    const history = { push: jest.fn() }
+    const wrapper = shallow(<AddExpensePage onSumbit={onSubmit} history={history} />)
+    expect(wrapper).toMatchSnapshot()
+})
+
