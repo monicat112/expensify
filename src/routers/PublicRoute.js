8de27f350341649c1d8a0d6b858ab32444374b20
Monica Salter <salter@smartdeploy.com>
2019-02-06 13:47:33 -0800

    Created public and private routes

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,23 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
-import Header from '../components/Header'
 
 // see Private Routes in notes/section-16-firebase-auth.md
 
-export const PrivateRoute = ({ 
+export const PublicRoute = ({ 
     isAuthenticated,
     component: Component,
     ...rest
 }) => (
     <Route {...rest} component={(props) => (
         isAuthenticated ? (
-            <div>
-                <Header />
-                <Component {...props} />
-            </div>
+            <Redirect to="/dashboard" />
         ) : (
-            <Redirect to="/" />
+            <Component {...props} />
         )
     )} />
 )
@@ -26,4 +22,4 @@ const mapStateToProps = (state) => ({
     isAuthenticated: !!state.auth.uid
 })
 
-export default connect(mapStateToProps)(PrivateRoute)
+export default connect(mapStateToProps)(PublicRoute)
