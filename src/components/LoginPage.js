b67f72fa7acd360ca5a55b3fae1ab979eeb71e28
Monica Salter <salter@smartdeploy.com>
2019-02-11 15:49:24 -0800

    Text and image changes

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
-            <p>It's time to get your expenses under control.</p>
+            <p>Track your expenses</p>
             <button className="button" onClick={startLogin}>Login with Google</button>
         </div>
     </div>
